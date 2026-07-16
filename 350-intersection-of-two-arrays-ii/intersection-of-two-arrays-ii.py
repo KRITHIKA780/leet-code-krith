ebf9a8e07d9a038c44aefc2d127ee998c0e99a74
KRITHIKA N <Kumar641037@gmail.com>
2026-07-16 21:05:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (66.08%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -2,16 +2,11 @@ class Solution(object):
     def intersect(self, nums1, nums2):
         count = {}
         result = []
-
-        # Count frequency of nums1
         for num in nums1:
             count[num] = count.get(num, 0) + 1
-
-        # Find common elements
         for num in nums2:
             if count.get(num, 0) > 0:
                 result.append(num)
                 count[num] -= 1
-
         return result
         
