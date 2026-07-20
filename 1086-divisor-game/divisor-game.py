aaf8413ed3397290fecee5184ccc6ddde7709f0a
KRITHIKA N <Kumar641037@gmail.com>
2026-07-20 20:35:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (89.16%) - LeetSync

--- 1086-divisor-game/divisor-game.py
+++ 1086-divisor-game/divisor-game.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def divisorGame(self, n):
-        if n % 2 == 0:
-            return True
-        else:
-            return False
-        
+       if n%2==0:
+         return True
+       else:
+         return False 
